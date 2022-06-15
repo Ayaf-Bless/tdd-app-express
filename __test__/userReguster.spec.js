7cc85c7cc5f9fabc521094e0eed7a0c3e064f697
Ayaf-Bless <blessambel1@gmail.com>
2022-06-16 00:15:12 +0300

    finilize test for the user registration

--- __test__/userReguster.spec.js
+++ __test__/userReguster.spec.js
@@ -1,16 +1,32 @@
 const request = require("supertest");
-const app = require("../app");
+const app = require("../src/app");
 
-it("returns 200 when signup is valid", (done) => {
-  request(app)
-    .post("/api/1.0/users")
-    .send({
-      username: "user1",
-      email: "email@gmail.com",
-      password: "1122",
-    })
-    .then((res) => {
-      expect(res.status).toBe(200);
-      done();
-    });
+describe("user registration", () => {
+  it("returns 200 when signup is valid", (done) => {
+    request(app)
+      .post("/api/1.0/users")
+      .send({
+        username: "user1",
+        email: "test@gmail.com",
+        password: "1122",
+      })
+      .then((res) => {
+        expect(res.status).toBe(200);
+        done();
+      });
+  });
+
+  it("returns success message when the data is valid", (done) => {
+    request(app)
+      .post("/api/1.0/users")
+      .send({
+        username: "user1",
+        email: "test@gmail.com",
+        password: "1122",
+      })
+      .then((res) => {
+        expect(res.body.message).toBe("User created");
+        done();
+      });
+  });
 });
--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.post("/api/1.0/users", (req, res) => {
-  return res.status(200).send();
+  res.status(200).send({ message: "User created" });
 });
 
 module.exports = app;
